a122d61c116bdea86d62529d3234c800d7d0f0a9
saifawin <saifa.win@student.mahidol.ac.th>
2018-05-03 18:27:18 +0700

    change2

--- CarParking Android/config.js
+++ CarParking Android/config.js
@@ -1,2 +1,4 @@
 /*var ip_address='http://192.168.43.98/CarParking/';*/
-var ip_address='http://localhost:4737/';
+/*var ip_address='http://localhost:4737/';*/
+
+var ip_address='http://localhost:90/';
